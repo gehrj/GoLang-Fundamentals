bb5f16608500ce499a37e113ff0762b094eed50e
Justin Gehr <jgehr@fluidpay.com>
2018-07-19 20:48:26 -0500

    moving on to maps

---
+++ 18_slices/08_comparing_shorthand_var_make/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	student := []string{}
+	students := [][]string{}
+	student[0] = "Justin"
+	// student = append(student,)
+	fmt.Println(student)
+	fmt.Println(students)
+}
